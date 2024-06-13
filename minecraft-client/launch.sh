34359d808f2a6014f62b2fd9ccb881379e47c6d0
Log4JExploit <63979831+Log4JExploit@users.noreply.github.com>
2024-06-13 19:23:33 +0000

    Update launch.sh

--- minecraft-client/launch.sh
+++ minecraft-client/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+xhost +
 export MESA_LOADER_DRIVER_OVERRIDE=radeonsi
 sudo docker run -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -e DISPLAY=$DISPLAY mc:latest
